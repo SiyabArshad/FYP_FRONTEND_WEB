c5bb3d0854212158cfb74b722493e29ff78e1714
SiyabArshad <siyabarshadsatti@gmail.com>
2023-07-23 17:15:12 +0500

    reset password bug fixed

--- src/pages/ResetPassword.jsx
+++ src/pages/ResetPassword.jsx
@@ -1,38 +1,61 @@
-import React ,{isValidElement, useState}from 'react'
-import {TextField, Button} from "@mui/material";
-import Header from '../components/Header';
-import http from '../utils/http';
-import { useSelector,useDispatch } from 'react-redux';
-import { useParams } from 'react-router-dom';
-
+import React, { isValidElement, useState } from "react";
+import { TextField, Button } from "@mui/material";
+import Header from "../components/Header";
+import http from "../utils/http";
+import { useSelector, useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 export default function ResetPassword() {
-    const [password, setPassword] = useState("");
-    const [indicator,setindicator]=useState(false)
-    // const {isAuthenticated,currentUser}=useSelector((state)=>state.auth)
-    const {id} =useParams()
-    const HandleSubmit = async() => {
-            setindicator(true)
-        try{
-              if(isValidElement(password))
-              {
-                const {data}=await http.post("/reset",{password,token:id})
-                alert(data?.message)
-            }
-        }
-        catch{
-            alert("Tryagain later")
-        }
-        finally{
-            setindicator(false)
-        }   
-      };
+  const [password, setPassword] = useState("");
+  const [indicator, setindicator] = useState(false);
+  // const {isAuthenticated,currentUser}=useSelector((state)=>state.auth)
+  const { id } = useParams();
+  const HandleSubmit = async () => {
+    setindicator(true);
+    try {
+      if (password.length > 5) {
+        const { data } = await http.put(
+          `/newpassword`,
+          { password: password },
+          {
+            headers: {
+              token: id,
+            },
+          }
+        );
+        alert(data?.message);
+      } else {
+        alert("Paswword should be strong");
+      }
+    } catch (e) {
+      console.log(e);
+      alert("Tryagain later");
+    } finally {
+      setindicator(false);
+    }
+  };
   return (
     <>
-    <div style={{height:"100vh",width:"100vw",display:"flex",flexDirection:"column",alignItems:"center",overflow:"hidden"}}>
-    <Header/>
-        <div style={{display:"flex",flexDirection:"column",marginTop:"10rem"}}>
-        <TextField
-        sx={{marginBottom:1,width:300}}
+      <div
+        style={{
+          height: "100vh",
+          width: "100vw",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          overflow: "hidden",
+        }}
+      >
+        <Header />
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            marginTop: "10rem",
+          }}
+        >
+          <TextField
+            sx={{ marginBottom: 1, width: 300 }}
             value={password}
             className="outlined-basic"
             label="Password"
@@ -41,16 +64,17 @@ export default function ResetPassword() {
               setPassword(e.target.value);
             }}
           />
-              <Button
-        disabled={indicator}
-          onClick={HandleSubmit}
-          className="loginBtn"
-          variant="contained"
-          color="success">
+          <Button
+            disabled={indicator}
+            onClick={HandleSubmit}
+            className="loginBtn"
+            variant="contained"
+            color="success"
+          >
             Reset
-        </Button>
+          </Button>
         </div>
-    </div>
+      </div>
     </>
-  )
+  );
 }
